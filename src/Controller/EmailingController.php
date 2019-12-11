f8f7729f8cb1ba65f26bf9e51ad65503e602fa9e
XPS13Window <rogerhiufungcheng@gmail.com>
2019-12-11 21:22:06 +1100

    Google ReCaptcha Test

--- src/Controller/EmailingController.php
+++ src/Controller/EmailingController.php
@@ -9,12 +9,25 @@ class EmailingController extends AppController
 {
     public function email()
     {
+        
         $email = new EmailForm();
         $var1 = "Yo";
         $var2 = "Testing";
         $var3 = "Hi Hi";
         $url = "http://ie.infotech.monash.edu/foundation-system-build/app/";
         if ($this->request->is('post')) {
+            $recaptcha = $this->request['g-recaptcha-response'];
+ 
+        $google_url = "https://www.google.com/recaptcha/api/siteverify";
+        $secret = '6LeIxAcTAAAAAGG-vFI1TnRWxMZNFuojJ4WifJWe';
+        $ip = $_SERVER['REMOTE_ADDR'];
+        $url = $google_url . "?secret=" . $secret . "&response=" . $recaptcha ."&remoteip=" . $ip;
+        require_once(ROOT . DS. 'vendor' . DS . 'curl.php');
+        $res = getCurlData($url);
+        $res = json_decode($res, true);
+        
+        if(empty($res['success'])){
+        
             $request = $this->request->getData();
             $callback = new Email();
             $callback
@@ -28,6 +41,7 @@ class EmailingController extends AppController
                 ->send();
             //If you want to send without template, just pure text, use this
             //->send($var1);
+        }
         }
         $this->set(compact( 'email'));
     }
--- src/Template/Emailing/email.ctp
+++ src/Template/Emailing/email.ctp
@@ -1,7 +1,11 @@
+<script src="https://www.google.com/recaptcha/api.js"></script>
+
 <?php
 Echo "Enter your email address (will receive a random email from me";
 echo $this->Form->create($email,['type'=>'post','class'=>'form']);
-echo $this->Form->control('email');
+echo $this->Form->control('email');?>
+<div class="g-recaptcha" data-sitekey="6LeIxAcTAAAAAJcZVRqyHh71UMIEGNQ_MXjiZKhI"></div>
+<?php
 echo $this->Form->button("Sending",['class'=>'submit']);
 echo $this->Form->end();
 ?>
